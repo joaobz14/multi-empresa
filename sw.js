11de0d0503e886d9677ca064f5e02c704c29d615
joaobz14 <joaovictor_itajobi@hotmail.com>
2026-06-17 12:07:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE = "assist-multi-v1";
+const ASSETS = [
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => !a.endsWith(".png") || false)
+      .concat(ASSETS)
+      .filter((v, i, arr) => arr.indexOf(v) === i)
+    ).catch(() => caches.open(CACHE).then(c => c.add("./index.html")))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
+
+self.addEventListener("message", e => {
+  if (e.data === "SKIP_WAITING") self.skipWaiting();
+});
